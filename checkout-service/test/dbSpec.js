48f9a562a3f057c9622a947946df1e6f58c744b5
Alek Ortiz <aleksebastiano@gmail.com>
2021-01-23 19:45:30 -0600

    Added db tests

--- checkout-service/test/dbSpec.js
+++ checkout-service/test/dbSpec.js
@@ -1,50 +1,49 @@
-const { expect } = require('chai');
-const { assert } = require('chai');
+const { expect, assert } = require('chai');
 const createFakeData = require('../database/seed/dataGenerator.js');
-const DbSeed = require('../database/seed/populate.js');
-const Mongoose = require('mongoose');
-const Db = 'mongodb://localhost/prinventory';
+const mongoose = require('mongoose');
+const db = 'mongodb://localhost/prinventory';
 const Prinventory = require('../database/index.js');
+const mongooseOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+};
 
 describe('Database seeding', () => {
 
+  let fakeData;
+
   describe('Generate fake data', () => {
-    it('should generate 100 fake primary records with the correct structure with properties of the correct type to be used for database seed', () => {
 
-      let fakeData = createFakeData();
+    it('should generate 100 fake primary records with the correct structure and properties of the correct type to be used for database seed', () => {
 
-      assert.lengthOf(fakeData, 100, '100 items generated');
+      fakeData = createFakeData();
+
+      assert.lengthOf(fakeData, 100, 'fakeData contains 100 items');
+
       fakeData.forEach(fakeProduct => {
         assert.hasAllKeys(fakeProduct, ['id', 'price', 'inventory'], 'Contains the correct structure per database schema');
-        assert.typeOf(fakeProduct.id, 'number', 'id property is a number');
-        assert.typeOf(fakeProduct.price, 'number', 'price property is a number');
-        assert.typeOf(fakeProduct.inventory, 'number', 'inventory property is a number');
+        assert.typeOf(fakeProduct.id, 'number', 'property should be of type number');
+        assert.typeOf(fakeProduct.price, 'number', 'property should be of type number');
+        assert.typeOf(fakeProduct.inventory, 'number', 'property should be of type number');
       });
     });
   });
 
-  beforeEach( async () => {
-    await Mongoose.connect(Db);
-    await Prinventory.db.dropDatabase();
-  });
+  describe('Seeding database with fake data', () => {
 
-  describe('Populating database with fake data', () => {
+    before(async () => {
+      await Prinventory.db.dropDatabase();
+    });
 
-    it('should respond to a query', async () => {
-      await DbSeed();
-      await Mongoose.connect(Db);
-      let queryResult =  await Prinventory.find();
-      // Mongoose.connection.close();
-      assert.typeOf(queryResult, 'array', 'queryResult result is an array');
-      assert.lengthOf(queryResult, 100);
-      queryResult.forEach(fakeProduct => {
-        assert.containsAllKeys(fakeProduct, ["id", "price", "inventory"], 'Contains all relevant keys');
-        assert.typeOf(fakeProduct.id, 'number', 'id property is a number');
-        assert.typeOf(fakeProduct.price, 'number', 'price property is a number');
-        assert.typeOf(fakeProduct.inventory, 'number', 'inventory property is a number');
-      });
+    it('should contain 100 records after seeding', async () => {
+      await Prinventory.insertMany(fakeData);
+      let dbRecordsCount = await Prinventory.countDocuments();
+      assert(dbRecordsCount === 100, 'record count should be 100');
+    });
+
+    after(async () => {
+      await mongoose.connection.close();
     });
   });
-
-
 });
