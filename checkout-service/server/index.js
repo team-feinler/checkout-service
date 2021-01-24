2117832ec72ed70b0ec0acc553f8050782bb3ffc
Alek Ortiz <aleksebastiano@gmail.com>
2021-01-23 22:44:21 -0600

    Added API endpoints and tests. Slightly refactored server to handle invalid requests. Refactored datagenerator.js to display correct ids 1000 - 1099 instead of 1 - 100

--- checkout-service/server/index.js
+++ checkout-service/server/index.js
@@ -11,13 +11,20 @@ app.use(express.static(__dirname + '/../client/dist'));
 app.get('/priceandinventory/id/:productId', (req, res) => {
   let productId = req.params.productId;
   dbQuery.getProductPriceAndInventoryCount(productId)
-  .then((productInfo) => res.status(200).send(productInfo));
+  .then((productInfo) => {
+    // Function returns stringified empty array '[]' which has length of 2
+    if (productInfo.length === 2) {
+      res.status(404).send('Invalid product id');
+    } else {
+      res.status(200).send(productInfo);
+    }
+  });
 });
 
 app.post('/priceandinventory/id/multiple', (req, res) => {
   let productIds = req.body;
   if (productIds.length > 30 || productIds.length === 0 || !productIds) {
-    res.status(400);
+    res.status(400).end();
   } else {
     dbQuery.getMultipleProductsPriceAndInventoryCount(productIds)
     .then(productsInfo => res.status(200).send(JSON.stringify(productsInfo)));
@@ -26,3 +33,6 @@ app.post('/priceandinventory/id/multiple', (req, res) => {
 
 
 app.listen(port, () => console.log(`listening on port ${port}`));
+
+
+module.exports = app;
