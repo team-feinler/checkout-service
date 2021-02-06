c74bfd32786c339d19663b703e3211d3c0695946
Alek Ortiz <aleksebastiano@gmail.com>
2021-02-06 00:02:01 -0600

    Complete AddToListDropDown basic styling - Dropdown styling WIP

--- checkout-service/client/src/components/AddToListDropDown.jsx
+++ checkout-service/client/src/components/AddToListDropDown.jsx
@@ -23,11 +23,22 @@ const AddToListSection = styled.span`
   border-color: #adb1b8;
   height: 29px;
   padding-left: 5%;
+  &:hover {
+    background-color: #e2e5ea;
+  }
 `;
 
 const DropDownArrowSection = styled.span`
-  width: 13%;
+  position: absolute;
+  width: 26px;
   height: 29px;
+  &:hover {
+    background-color: #e2e5ea;
+  };
+  &:focus {
+    box-shadow: 0 0 3px 2px rgb(228 121 17 / 50%);
+    outline: 0;
+  };
 `;
 
 const DropDownArrowIcon = styled.i`
@@ -37,16 +48,50 @@ const DropDownArrowIcon = styled.i`
   width: 10px;
   background-image: url("https://m.media-amazon.com/images/S/sash/McBZv0ZvnbehkIx.png");
   background-position: -7px -102px;
+  border: none;
 `;
 
-// const DropDown = styled.div`
+const DropDownListContainer = styled("div")``;
 
-// `;
+const DropDownList = styled("ul")`
+  padding: 0;
+  margin: 0;
+  padding-left: 1em;
+  background: #ffffff;
+  border: 2px solid #e5e5e5;
+  box-sizing: border-box;
+  color: #3faffa;
+  font-size: 1.3rem;
+  font-weight: 500;
+  &:first-child {
+    margin-top: 0.1em;
+    /* padding-top: 0.8em; */
+  }
+`;
+
+const ListItem = styled("li")`
+  list-style: none;
+  /* margin-bottom: 0.8em; */
+  height: 29px;
+  border: 1px solid black;
+`;
 
 const AddToListDropDown = (props) => {
+  const [dropDown, setDropDown] = useState(false);
+  const toggleDropDown = () => setDropDown(!dropDown);
+
   return <AddToListDropDownWrapper>
     <AddToListSection>Add to List</AddToListSection>
-    <DropDownArrowSection><DropDownArrowIcon></DropDownArrowIcon></DropDownArrowSection>
+    <DropDownArrowSection onFocus={toggleDropDown} onBlur={toggleDropDown} tabIndex="0"><DropDownArrowIcon></DropDownArrowIcon></DropDownArrowSection>
+    {dropDown && (
+      <DropDownListContainer>
+        <DropDownList>
+          <ListItem>Mangoes</ListItem>
+          <ListItem>Apples</ListItem>
+          <ListItem>Oranges</ListItem>
+        </DropDownList>
+      </DropDownListContainer>
+    )}
   </AddToListDropDownWrapper>
 
 };
