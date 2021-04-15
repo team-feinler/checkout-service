42ff1a188b8eecbe103c81a1e7d34af30205d20e
Mason Pott <masoncpott@gmail.com>
2021-04-14 22:15:01 -0500

    removed console logs and cleaned out commented code

--- checkout-service/server/index.js
+++ checkout-service/server/index.js
@@ -4,7 +4,6 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const port = 4003;
 const app = express();
-// const dbQuery = require('../database/query.js'); // <-- mongoDB query functions
 const {
   getProductPriceAndInventoryCount,
   getMultipleProductsPriceAndInventoryCount,
@@ -13,6 +12,7 @@ const {
   createNewRecord
 } = require('../database/postgres/postgresModel.js');
 
+//Middlewares/setup
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -24,14 +24,13 @@ var corsOptions = {
   optionsSuccessStatus: 200
 };
 
-
+//routes
 app.get('/priceandinventory/id/:productId', (req, res) => {
   let productId = req.params.productId;
-  // dbQuery.getProductPriceAndInventoryCount(productId) // <-- mongoDB query
   getProductPriceAndInventoryCount(productId)
   .then((productInfo) => {
     if (!productInfo.length) {
-      res.status(404).send('Invalid product id');
+      res.status(500).send('Invalid product id');
     } else {
       res.status(200).send(productInfo);
     }
@@ -41,9 +40,8 @@ app.get('/priceandinventory/id/:productId', (req, res) => {
 app.post('/priceandinventory/id/multiple', (req, res) => {
   let productIds = req.body;
   if (productIds.length > 30 || productIds.length === 0 || !productIds) {
-    res.status(400).end();
+    res.status(500).end();
   } else {
-    // dbQuery.getMultipleProductsPriceAndInventoryCount(productIds)
     getMultipleProductsPriceAndInventoryCount(productIds)
     .then(productsInfo => res.status(200).send(productsInfo));
   }
@@ -51,42 +49,36 @@ app.post('/priceandinventory/id/multiple', (req, res) => {
 
 app.post('/priceandinventory/id/createRecord', (req, res) => {
   let newRecord = req.body;
-  // dbQuery.createNewRecord(newRecord)
   createNewRecord(newRecord)
     .then(() => {
       res.sendStatus(200);
     })
     .catch((error) => {
-      console.log('ERROR SAVING RECORD in server: ', error);
-      res.status(400).end();
+      res.status(500).end();
     })
-})
+});
 
 app.put('/priceandinventory/id/updateRecord', (req, res) => {
   let recordToUpdate = req.body;
-  // dbQuery.updateOneRecord(recordToUpdate)
   updateOneRecord(recordToUpdate)
     .then((result) => {
       res.status(200).send(result);
     })
     .catch((error) => {
-      console.log('FAILED TO UPDATE in server: ', error);
-      res.status(400).end();
+      res.status(500).end();
     })
-})
+});
 
 app.delete('/priceandinventory/id/removeRecord/:productId', (req, res) => {
   let { productId } = req.params;
-  // dbQuery.removeOneRecord(productId)
   removeOneRecord(productId)
     .then(() => {
       res.sendStatus(200);
     })
     .catch((err) => {
-      console.log('FAILED DELETING RECORD IN SERVER', err);
-      res.status(400).end();
+      res.status(500).end();
     })
-})
+});
 
 app.listen(port, () => console.log(`listening on port ${port}`));
 
